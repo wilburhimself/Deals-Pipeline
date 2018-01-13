a941724b491ae109064d09be62f22579b2ccd04c
Wilbur Suero <wilbur.himself@gmail.com>
2018-01-13 01:15:11 -0400

    add styles to chart

--- app/javascript/packs/deals.js
+++ app/javascript/packs/deals.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './deals/components/App'
 
-
 const deals = document.querySelector('#deals')
 ReactDOM.render(<App />, deals)
--- app/javascript/packs/deals/components/App.jsx
+++ app/javascript/packs/deals/components/App.jsx
@@ -3,86 +3,99 @@ import axios from 'axios'
 import { Bar } from 'react-chartjs-2'
 
 class App extends React.Component {
-    constructor () {
-        super()
-        this.state = {
-            labels: [],
-            values: []
+  constructor () {
+    super()
+    this.state = {
+      labels: [],
+      values: []
+    }
+  }
+
+  fetchDeals () {
+    axios.get('api/deals')
+      .then(response => {
+        this.setState({
+          labels: response.data.labels.map((label) => { return label.name }),
+          values: response.data.values.map((value) => { return value.value.toFixed(2) })
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  componentDidMount () {
+    this.fetchDeals()
+  }
+
+  componentWillReceiveProps () {
+    this.fetchDeals()
+  }
+
+  render () {
+    const data = {
+      labels: this.state.labels,
+      datasets: [
+        {
+          label: 'Deals Pipeline',
+          backgroundColor: 'rgba(0,150,255,0.2)',
+          borderColor: 'rgba(0,150,255,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(0,150,255,0.4)',
+          hoverBorderColor: 'rgba(0,150,255,1)',
+          data: this.state.values
         }
+      ]
     }
 
-    fetchDeals () {
-        axios.get('api/deals')
-            .then(response => {
-                console.log(response)
-                this.setState({
-                    labels: response.data.labels.map((label) => { return label.name }),
-                    values: response.data.values.map((value) => { return value.value.toFixed(2) })
-                })
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    }
-
-    componentDidMount () {
-        this.fetchDeals()
-    }
-
-    componentWillReceiveProps () {
-        this.fetchDeals()
-    }
-
-    render () {
-        const data = {
-            labels: this.state.labels,
-            datasets: [
-                {
-                    label: 'Deals Pipeline',
-                    // backgroundColor: 'rgba(255,99,132,0.2)',
-                    // borderColor: 'rgba(255,99,132,1)',
-                    // borderWidth: 1,
-                    // hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                    // hoverBorderColor: 'rgba(255,99,132,1)',
-                    data: this.state.values
-                }
-
-            ]
-        }
-
-        const options = {
-            legend: {
-                display: false
+    const options = {
+      legend: {
+        display: false
+      },
+      tooltips: {
+        mode: 'label',
+        label: 'mylabel',
+        callbacks: {
+          label: function(tooltipItem, data) {
+            return '$' + tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          },
+        },
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            callback: function (label, index, labels) {
+              return '$' + label.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             },
-            tooltips: {
-                mode: 'label',
-                label: 'mylabel',
-                callbacks: {
-                    label: function(tooltipItem, data) {
-                        return '$' + tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); }, },
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        callback: function (label, index, labels) {
-                            return '$' + label.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                        },
-                        beginAtZero: true,
-                        fontSize: 10,
-                    },
-                    gridLines: {
-                        display: true
-                    }
-                }],
-            }
-
-            }
-        return (
-            <div>
-                <Bar data={data} options={options} />
-            </div>
-        )
+            beginAtZero: true,
+            fontSize: 10,
+          },
+          gridLines: {
+            display: true
+          }
+        }],
+      }
     }
+
+    let styles= {
+      chartStyle: {
+        width: '55%',
+        margin: '0 auto'
+      }
+    }
+
+    return (
+      <div>
+        <div style={styles.chartStyle}>
+          <header>
+            <h3>Deals Pipeline</h3>
+            <p>Total $ value of deals by stage</p>
+          </header>
+          <Bar data={data} options={options} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
