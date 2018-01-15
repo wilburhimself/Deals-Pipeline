e26713c0ea8a3075315750731c8859d1d271fdde
Wilbur Suero <wilbur.himself@gmail.com>
2018-01-15 09:27:39 -0400

    cleanup deals data spec and ficture

--- spec/controllers/api/deals_controller_spec.rb
+++ spec/controllers/api/deals_controller_spec.rb
@@ -21,4 +21,19 @@ RSpec.describe Api::DealsController, type: :controller do
       expect(json['values'].length).to eq(4)
     end
   end
+
+  describe 'chart data' do
+    before do
+      get :index, format: :json
+    end
+
+    it 'brings an ordered list of labels' do
+      # order is based on deal percentage
+      expect(json['labels']).to eq(["Request for Info", "Presentation", "Negotiation", "Won"])
+    end
+
+    it 'brings the correct sum of values for each label' do
+      expect(json['values']).to eq([100.0, 100.0, 200.0, 100.0])
+    end
+  end
 end
